c6aa78671aedabb4fae5d5c85f1da16ceb4c20e1
JoshChuah <70768243+JoshChuah@users.noreply.github.com>
2020-12-04 11:45:23 -0500

    Update Final_EDA.R

--- Final_EDA.R
+++ Final_EDA.R
@@ -1,5 +1,8 @@
 ###Final EDA
 
+
+
+####Load in Data and Separate By Indicator
 EDA <- read.csv("finalEDA.csv", header = TRUE)
 
 ANC <- EDA[EDA$Indicator == "Antenatal care 1+ visit - percentage of women (aged 15-49 years) attended at least once during pregnancy by skilled health personnel",]
@@ -8,7 +11,7 @@ NMR <- EDA[EDA$Indicator == "Neonatal mortality rate",]
 WAT <- EDA[EDA$Indicator == "Proportion of population using improved drinking water sources",]
 SANI <- EDA[EDA$Indicator == "Proportion of population using improved sanitation facilities",]
 SHP <- read.csv("SHP_2014.csv", header = TRUE)
-
+LBW <- EDA[EDA$Indicator == "Prevalence of low birth weight among new-borns",]
 
 
 GDP <- read.csv("GDP.csv", header=TRUE, skip=4)
@@ -16,6 +19,161 @@ f_GDP <- GDP[GDP$Country.Name %in% sub_countries,]
 df_GDP <- data.frame("Country"=f_GDP$Country.Name, "Value" = f_GDP$X2014)
 df_GDP <- df_GDP[order(df_GDP$Country),]
 df_GDP
+
+
+#######
+##Hypothesis Testing
+#######
+library(ggplot2)
+over <- NMR[NMR$OBS_VALUE > 12,]$Geographic.area
+under <- NMR[NMR$OBS_VALUE <= 12,]$Geographic.area
+
+over_ANC <- ANC[ANC$Geographic.area %in% over,]$OBS_VALUE
+under_ANC <- ANC[ANC$Geographic.area %in% under,]$OBS_VALUE
+wilcox.test(over_ANC, under_ANC)
+t.test(over_ANC, under_ANC)
+var.test(over_ANC, under_ANC)
+
+a <- data.frame(group = "high NMR", value = over_ANC)
+b <- data.frame(group = "low NMR", value = under_ANC)
+
+plot.data <- rbind(a,b)
+ggplot(plot.data, aes(x=group, y=value, fill=group)) + geom_boxplot() + stat_summary(fun=mean, geom="point", shape=20, size=10, color="red", fill="red")
+
+
+over_IB <- IB[IB$Geographic.area %in% over,]$OBS_VALUE
+under_IB <- IB[IB$Geographic.area %in% under,]$OBS_VALUE
+wilcox.test(over_IB, under_IB)
+t.test(over_IB, under_IB)
+var.test(over_IB, under_IB)
+
+a <- data.frame(group = "high NMR", value = over_IB)
+b <- data.frame(group = "low NMR", value = under_IB)
+
+plot.data <- rbind(a,b)
+ggplot(plot.data, aes(x=group, y=value, fill=group)) + geom_boxplot() + stat_summary(fun=mean, geom="point", shape=20, size=10, color="red", fill="red")
+
+over_WAT <- WAT[WAT$Geographic.area %in% over,]$OBS_VALUE
+under_WAT <- WAT[WAT$Geographic.area %in% under,]$OBS_VALUE
+wilcox.test(over_WAT, under_WAT)
+t.test(over_WAT, under_WAT)
+var.test(over_WAT, under_WAT)
+
+a <- data.frame(group = "high NMR", value = over_WAT)
+b <- data.frame(group = "low NMR", value = under_WAT)
+
+plot.data <- rbind(a,b)
+ggplot(plot.data, aes(x=group, y=value, fill=group)) + geom_boxplot() + stat_summary(fun=mean, geom="point", shape=20, size=10, color="red", fill="red")
+
+over_SANI <- SANI[SANI$Geographic.area %in% over,]$OBS_VALUE
+under_SANI <- SANI[SANI$Geographic.area %in% under,]$OBS_VALUE
+wilcox.test(over_SANI, under_SANI)
+t.test(over_SANI, under_SANI)
+var.test(over_SANI, under_SANI)
+
+a <- data.frame(group = "high NMR", value = over_SANI)
+b <- data.frame(group = "low NMR", value = under_SANI)
+
+plot.data <- rbind(a,b)
+ggplot(plot.data, aes(x=group, y=value, fill=group)) + geom_boxplot() + stat_summary(fun=mean, geom="point", shape=20, size=10, color="red", fill="red")
+
+over_LBW <- LBW[LBW$Geographic.area %in% over,]$OBS_VALUE
+under_LBW <- LBW[LBW$Geographic.area %in% under,]$OBS_VALUE
+wilcox.test(over_LBW, under_LBW)
+t.test(over_LBW, under_LBW)
+var.test(over_LBW, under_LBW)
+
+a <- data.frame(group = "high NMR", value = over_LBW)
+b <- data.frame(group = "low NMR", value = under_LBW)
+
+plot.data <- rbind(a,b)
+ggplot(plot.data, aes(x=group, y=value, fill=group)) + geom_boxplot() + stat_summary(fun=mean, geom="point", shape=20, size=10, color="red", fill="red")
+
+
+
+#########
+####Using Full Model First (all variables)
+#########
+intersection <- Reduce(intersect, list(ANC$Geographic.area, IB$Geographic.area, WAT$Geographic.area, SANI$Geographic.area, LBW$Geographic.area, GDP$Country.Name, SHP$Country))
+NMR_int <- NMR[NMR$Geographic.area %in% intersection,]
+full_labels <- vector(mode="character", length=length(intersection))
+for(val in 1:length(full_labels)){
+  if(NMR_int$Geographic.area[val] %in% over){
+    full_labels[val] = "over"
+  }
+  else{
+    full_labels[val] = "under"
+  }
+}
+full_labels
+
+full_GDP <- df_GDP[df_GDP$Country %in% intersection,"Value"]
+full_NMR <- df_NMR[df_NMR$Geographic.area %in% intersection,c("Geographic.area","OBS_VALUE")]
+full_ANC <- df_ANC[df_ANC$Geographic.area %in% intersection,"OBS_VALUE"]
+full_IB  <- df_IB[df_IB$Geographic.area %in% intersection,"OBS_VALUE"]
+full_WAT <- df_WAT[df_WAT$Geographic.area %in% intersection,"OBS_VALUE"]
+full_SHP <- df_SHP[df_SHP$Country %in% intersection,"Births.attended.by.skilled.health.personnel...."]
+full_SANI<- df_SANI[df_SANI$Geographic.area %in% intersection,"OBS_VALUE"]
+full_LBW <- df_LBW[df_LBW$Geographic.area %in% intersection,"OBS_VALUE"]
+full_data<- data.frame(full_NMR$Geographic.area, full_NMR$OBS_VALUE,full_ANC,full_IB,full_WAT,full_SANI,full_LBW,full_SHP, full_GDP)
+colnames(full_data) <- data.frame("country", "NMR", "ANC", "IB", "WAT", "SANI", "LBW", "SHP", "GDP")
+
+
+full_cluster<- kmeans(full_data[,-c(1,2)],2,nstart=100, iter.max = 1000)
+table(full_cluster$cluster, full_labels)
+adjustedRandIndex(full_labels,full_cluster$cluster)
+
+s <- Reduce(intersect,list(LBW$Geographic.area, SHP$Country, IB$Geographic.area, SANI$Geographic.area, WAT$Geographic.area))
+
+s_NMR <- NMR[NMR$Geographic.area %in% s, "OBS_VALUE"]
+s_WAT <- WAT[WAT$Geographic.area %in% s, "OBS_VALUE"]
+s_SANI <- SANI[SANI$Geographic.area %in% s, "OBS_VALUE"]
+s_LBW <- LBW[LBW$Geographic.area %in% s, "OBS_VALUE"]
+s_IB <- IB[IB$Geographic.area %in% s, "OBS_VALUE"]
+s_SHP <- SHP[SHP$Country %in% s, "Births.attended.by.skilled.health.personnel...."]
+
+
+NMR_s <- NMR[NMR$Geographic.area %in% s,]
+s_labels <- vector(mode="character", length=length(s))
+for(val in 1:length(s_labels)){
+  if(NMR_s$Geographic.area[val] %in% over){
+    s_labels[val] = "over"
+  }
+  else{
+    s_labels[val] = "under"
+  }
+}
+
+s_data <- data.frame(NMR_s$Geographic.area, s_WAT, s_SANI, s_LBW, s_IB, s_SHP)
+colnames(s_data) <- data.frame("country", "WAT", "SANI", "LBW", "IB", "SHP")
+
+s_cluster<- kmeans(s_data[,-1],2,nstart=100, iter.max = 1000)
+table(s_cluster$cluster, s_labels)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+library(party)
+NMR_tree <- ctree(NMR ~ ANC+IB+WAT+SANI+LBW+SHP+GDP, data=full_data)
+plot(NMR_tree)
 over <- NMR[NMR$OBS_VALUE > 12,]$Geographic.area
 under <- NMR[NMR$OBS_VALUE <= 12,]$Geographic.area
 
@@ -33,6 +191,7 @@ df_IB <- df_IB[df_IB$TIME_PERIOD == "2014",]
 df_WAT <- WAT[WAT$Geographic.area %in% sub_countries,]
 df_SANI <- SANI[SANI$Geographic.area %in% sub_countries,]
 df_SHP <- SHP[SHP$Country %in% sub_countries,]
+df_LBW <- LBW[LBW$Geographic.area %in% sub_countries,]
 
 five_var <- df_SHP$Country
 five_NMR <- df_NMR[df_NMR$Geographic.area %in% five_var,]
@@ -49,6 +208,10 @@ boxplot(ANC_over,ANC_under)
 t.test(ANC_over,ANC_under)
 wilcox.test(ANC_over,ANC_under)
 
+high_contrib <- data.frame(full_NMR$Geographic.area,full_IB,full_WAT,full_SANI,full_LBW,full_SHP)
+
+full2_cluster<- kmeans(high_contrib[,-1],2,nstart=100, iter.max = 1000)
+table(full2_cluster$cluster, full_labels)
 
 
 expanded_data <- data.frame(five_var, df_SHP$Births.attended.by.skilled.health.personnel....,five_ANC$OBS_VALUE, five_IB$OBS_VALUE, five_SANI$OBS_VALUE, five_WAT$OBS_VALUE)
@@ -143,6 +306,10 @@ table(f2_cluster$cluster, f2_labels)
 e_class <- ifelse(f2_labels %in% c("over"), 1, 2)
 rand.index(e_class,f2_cluster$cluster)
 
+library(mclust)
+adjustedRandIndex(f2_cluster$cluster, f2_labels)
+adjustedRandIndex(g_cluster$cluster, GDP_labels)
+
 true_labels <- ifelse(f_labels %in% c("over"), 1, 2)
 rand.index(true_labels,f_cluster$cluster)
 
@@ -173,13 +340,13 @@ fviz_cluster(g_cluster, data = total_dataset[, -1],
 
 
 # Dimension reduction using PCA
-res.pca <- prcomp(total_dataset[, -1],  scale = TRUE)
+res.pca <- prcomp(full_data[, -c(1,2)],  scale = TRUE)
 # Coordinates of individuals
 ind.coord <- as.data.frame(get_pca_ind(res.pca)$coord)
 # Add clusters obtained using the K-means algorithm
-ind.coord$cluster <- factor(g_cluster$cluster)
+ind.coord$cluster <- factor(full_cluster$cluster)
 # Add Species groups from the original data sett
-ind.coord$labels <- GDP_labels
+ind.coord$labels <- full_labels
 # Data inspection
 
 # Percentage of variance explained by dimensions
@@ -188,7 +355,7 @@ variance.percent <- eigenvalue$variance.percent
 head(eigenvalue)
 var <- get_pca_var(res.pca)
 
-head(ind.coord)
+vhead(ind.coord)
 library(ggpubr)
 ggscatter(
   ind.coord, x = "Dim.1", y = "Dim.2", 
